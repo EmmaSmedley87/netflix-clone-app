a9ba75f0961cf37721814ee2637ca746c037fb7a
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2022-08-04 20:13:16 +0100

    First commit, created basic React app

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build Netflix Clone Front-end!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// 24.12
---
+++ script.js
@@ -0,0 +1,3 @@
+// https://api.themoviedb.org/3/movie/550?api_key=866f0e776e7f54d196665e39d38693df
+
+// API KEY 866f0e776e7f54d196665e39d38693df
